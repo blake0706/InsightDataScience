37bf905280fbe5583943312bf65c8031cce2d455
blake0706 <blake0706@users.noreply.github.com>
2018-06-19 15:37:29 -0400

    Fully implemented version

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# edgar
-batch processing Edgar data
+This is a straight forward Java implementation to process and analysis EDGAR data.
+
+# Implementation and test
+
+It was implemented with Java, It should run for any version of Java that is after Java 1.5. It included run.sh from root
+and run_tests.sh from insight_testsuite directories. Both of them were tested under MAC OS.
+
+# Unit tests
+
+As an experienced programmer, I agree with that we should create unit tests to prevent regressions. However for this
+project I don't really have class public member functions to test. My implementation only includes a main function with
+two private static helpers. Technically we should have unit test for them. But here I don't want to be that
+paranoid.
+
+# Integration tests
+
+I followed instructions and create a bunch of tests under test suites. However as mentioned in description we are not
+allowed to upload a file over 100M to github.
+
+# Optimization
+
+I realized that we could potentially have more optimizations from two places. One place is to read the log file a chunk
+instead of one line each time. We can read file into a buffered chunk and process it. Another place is to have separate
+priority queue to remember logs we have so far when we scan logs line by line, then we don't need to detect expired
+session each time we read one new log. We can peek priority queue to check if we need output a expired session. All of
+those optimizations involve more complicated algorithms and trade-off considerations. I think that is beyond the scope
+of this project. Would be happy to have further discussion on the matter.
--- src/Sessionization.java
+++ src/Sessionization.java
@@ -2,37 +2,30 @@ import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Sessionization {
     /**
-     *
-     * @param dateString
-     * @param timeString
-     * @return
+     * Convert to a long integer from date time string with format "yyyy-mm-ddhh:mm:ss".
+     * @param dateString date string
+     * @param timeString time string
+     * @return long integer value
      * @throws ParseException
      */
-    private static long dateTimeToSeconds(String dateString, String timeString) {
-        try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-ddhh:mm:ss");
-            Date date = dateFormat.parse(dateString + timeString);
-            return date.getTime() / 1000;
-        } catch (ParseException e) {
-
-        }
-
-        return -1;
+    private static long dateTimeToSeconds(String dateString, String timeString) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-ddhh:mm:ss");
+        Date date = dateFormat.parse(dateString + timeString);
+        return date.getTime() / 1000;
     }
 
     /**
-     *
-     * @param writer
-     * @param ipAddr
-     * @param sessionStartDate
-     * @param sessionStartTime
-     * @param sessionEndDate
-     * @param sessionEndTime
-     * @param sessionCounter
+     * Write a session to output.
+     * @param writer output writer
+     * @param ipAddr ip address string
+     * @param sessionStartDate start date string
+     * @param sessionStartTime start time string
+     * @param sessionEndDate end date string
+     * @param sessionEndTime end time string
+     * @param sessionCounter number of accesses with the session
      * @throws ParseException
      * @throws IOException
      */
@@ -59,8 +52,8 @@ public class Sessionization {
     }
 
     /**
-     *
-     * @param args
+     * Main entrance to start the app.
+     * @param args in the order of log file, inactivity_period and output files
      */
     public static void main(String[] args) {
         if (args.length < 3) {
@@ -86,52 +79,37 @@ public class Sessionization {
             Map<String, String> sessionEndDate = new HashMap<>();
             Map<String, String> sessionEndTime = new HashMap<>();
             Map<String, Integer> sessionCounter = new HashMap<>();
-            PriorityQueue<String> accessingQue = new PriorityQueue<>((ip1, ip2) -> {
-                String endDate1 = sessionEndDate.get(ip1);
-                String endDate2 = sessionEndDate.get(ip2);
-                if (endDate1 == null || endDate2 == null) {
-                    return 1;
-                }
-                String endTime1 = sessionEndTime.get(ip1);
-                String endTime2 = sessionEndTime.get(ip2);
-                long dt1 = dateTimeToSeconds(endDate1, endTime1);
-                long dt2 = dateTimeToSeconds(endDate2, endTime2);
-                if (dt1 != dt2) {
-                    return (int) (dt1 - dt2);
-                }
 
-                String startDate1 = sessionStartDate.get(ip1);
-                String startTime1 = sessionStartTime.get(ip1);
-                String startDate2 = sessionStartDate.get(ip2);
-                String startTime2 = sessionStartTime.get(ip2);
-                return (int) (dateTimeToSeconds(startDate1, startTime1) - dateTimeToSeconds(startDate2, startTime2));
-            });
+            Map<String, Integer> fieldOrder = new HashMap<>();
+            Queue<String> accessingQue = new LinkedList<>();
 
             String oneLog = null;
             String ipAddr = null;
             String date = null;
             String time = null;
 
-            // ip,date,time,zone,cik,accession,extention,code,size,idx,norefer,noagent,find,crawler,browser
-            reader.readLine();
+            // Read field names
+            String[] fieldNames = reader.readLine().split(SEPARATOR);
+            for (int i = 0; i < fieldNames.length; i++) {
+                fieldOrder.put(fieldNames[i], i);
+            }
+
             while ((oneLog = reader.readLine()) != null) {
-                String[] fieldValues = oneLog.split(",");
-                if (fieldValues.length != 15) {
-
+                String[] fieldValues = oneLog.split(SEPARATOR);
+                if (fieldValues.length < fieldOrder.get(TIME_STRING) + 1) {
+                    System.out.println("Log with incorrect log format found, ignore.");
+                    continue;
                 }
-                ipAddr = fieldValues[0];
-                date = fieldValues[1];
-                time = fieldValues[2];
+                ipAddr = fieldValues[fieldOrder.get(IP_ADDRESS)];
+                date = fieldValues[fieldOrder.get(DATE_STRING)];
+                time = fieldValues[fieldOrder.get(TIME_STRING)];
 
+                // Detect sessions that expired
                 for (Iterator<String> iterator = accessingQue.iterator(); iterator.hasNext();) {
                     String addr = iterator.next();
                     int timePassed = (int) (dateTimeToSeconds(date, time)
                             - dateTimeToSeconds(sessionEndDate.get(addr), sessionEndTime.get(addr)));
-                    if (timePassed < inactivity) {
-                        break;
-                    } else if (timePassed <= inactivity && addr.equals(ipAddr)) {
-                        break;
-                    }
+                    if (timePassed <= inactivity) continue;
 
                     writeSession(writer, addr, sessionStartDate.get(addr), sessionStartTime.get(addr),
                             sessionEndDate.get(addr), sessionEndTime.get(addr), sessionCounter.get(addr));
@@ -144,6 +122,7 @@ public class Sessionization {
                     sessionEndTime.remove(addr);
                 }
 
+                // Book/update one record
                 if (! sessionCounter.containsKey(ipAddr)) {
                     sessionCounter.put(ipAddr, 1);
                     sessionStartDate.put(ipAddr, date);
@@ -155,30 +134,11 @@ public class Sessionization {
                     sessionCounter.put(ipAddr, sessionCounter.get(ipAddr) + 1);
                     sessionEndDate.put(ipAddr, date);
                     sessionEndTime.put(ipAddr, time);
-                    accessingQue.remove(ipAddr);
-                    accessingQue.add(ipAddr);
                 }
             }
 
-            String[] ipAddresses = accessingQue.stream().collect(Collectors.toSet()).toArray(new String[0]);
-            Arrays.sort(ipAddresses, (ip1, ip2) -> {
-                String startDate1 = sessionStartDate.get(ip1);
-                String startTime1 = sessionStartTime.get(ip1);
-                String startDate2 = sessionStartDate.get(ip2);
-                String startTime2 = sessionStartTime.get(ip2);
-                long dt1 = dateTimeToSeconds(startDate1, startTime1);
-                long dt2 = dateTimeToSeconds(startDate2, startTime2);
-                if (dt1 != dt2) {
-                    return (int) (dt1 - dt2);
-                }
-
-                String endDate1 = sessionEndDate.get(ip1);
-                String endDate2 = sessionEndDate.get(ip2);
-                String endTime1 = sessionEndTime.get(ip1);
-                String endTime2 = sessionEndTime.get(ip2);
-                return (int) (dateTimeToSeconds(endDate1, endTime1) - dateTimeToSeconds(endDate2, endTime2));
-            });
-            for (String addr: ipAddresses) {
+            // Flush all records in the queue
+            for (String addr: accessingQue) {
                 writeSession(writer, addr, sessionStartDate.get(addr), sessionStartTime.get(addr),
                         sessionEndDate.get(addr),
                         sessionEndTime.get(addr), sessionCounter.get(addr));
@@ -188,11 +148,11 @@ public class Sessionization {
             writer.close();
             reader.close();
         } catch (FileNotFoundException e) {
-
+            System.out.println(e.getCause());
         } catch (IOException e) {
-
+            System.out.println(e.getCause());
         } catch (Exception e) {
-            System.out.println(e.getStackTrace());
+            System.out.println(e.getCause());
         } finally {
             if (reader != null) {
                 try {
@@ -210,5 +170,13 @@ public class Sessionization {
             }
         }
     }
+
+    private static final String SEPARATOR = ",";
+    private static final String IP_ADDRESS = "ip";
+    private static final String DATE_STRING = "date";
+    private static final String TIME_STRING = "time";
+    private static final String CIK_STRING = "cik";
+    private static final String ACCESSION_STRING = "accession";
+    private static final String EXTENSION_STRING = "extention";
 }
 
